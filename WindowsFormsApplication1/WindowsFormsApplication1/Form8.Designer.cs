8cc43e613dc7622b8be63a6b5a436b9682dc6e93
sunlupeng2020 <slp2060@163.com>
2020-12-02 10:05:41 +0800

    添加了章节联动

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form8.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCourse = new System.Windows.Forms.ComboBox();
+            this.cmbSection = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbCourse
+            // 
+            this.cmbCourse.FormattingEnabled = true;
+            this.cmbCourse.Location = new System.Drawing.Point(62, 107);
+            this.cmbCourse.Name = "cmbCourse";
+            this.cmbCourse.Size = new System.Drawing.Size(121, 20);
+            this.cmbCourse.TabIndex = 0;
+            this.cmbCourse.SelectedIndexChanged += new System.EventHandler(this.cmbCourse_SelectedIndexChanged);
+            // 
+            // cmbSection
+            // 
+            this.cmbSection.FormattingEnabled = true;
+            this.cmbSection.Location = new System.Drawing.Point(247, 107);
+            this.cmbSection.Name = "cmbSection";
+            this.cmbSection.Size = new System.Drawing.Size(121, 20);
+            this.cmbSection.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "章：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 348);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSection);
+            this.Controls.Add(this.cmbCourse);
+            this.Name = "Form8";
+            this.Text = "Form8";
+            this.Load += new System.EventHandler(this.Form8_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCourse;
+        private System.Windows.Forms.ComboBox cmbSection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form8.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void Form8_Load(object sender, EventArgs e)
+        {
+            //创建连接
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "server=localhost;database=paper;user=root;password=;port=3306";
+            //创建命令对象
+            MySqlCommand cmd = conn.CreateCommand();
+            string sql = "select id,chaptername from chapter where courseid=12 order by number";
+            cmd.CommandText = sql;
+            #region
+            //打开连接
+            //conn.Open();
+            ////创建DataReader对象
+            //MySqlDataReader dr = cmd.ExecuteReader();
+            //读取数据
+
+            //cmbCourse.DataSource = dr;
+
+            //while (dr.Read())//读取一行数据
+            //{
+         
+            //    cmbCourse.Items.Add(dr[1]);//添加到ComboBox
+            //}
+            //dr.Close();//关闭DataReader
+            //conn.Close();//关闭连接
+#endregion
+            //数据适配器对象
+            MySqlDataAdapter sda = new MySqlDataAdapter(cmd);//参数是命令对象
+            DataSet ds = new DataSet();//创建数据集对象
+            sda.Fill(ds);//把适配器得到的数据填充到数据集中
+            cmbCourse.DataSource = ds.Tables[0].DefaultView;//设置combobox的数据源
+            cmbCourse.ValueMember = "id";//设置值字段
+            cmbCourse.DisplayMember = "chaptername";//设置显示的文本对应的字段
+
+            //cmbCourse.SelectedIndex = 0;//默认选择第一项
+        }
+        private void cmbCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string chapterid = cmbCourse.SelectedValue.ToString();//得到选择的章的id号
+            label2.Text = chapterid;
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "server=localhost;database=paper;user=root;password=;port=3306";
+            //创建命令对象
+            MySqlCommand cmd = conn.CreateCommand();
+            string sql = "select id,sectionname from section  where chapterid='" + chapterid + "' order by number";
+            cmd.CommandText = sql;
+            MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            cmbSection.DataSource = ds.Tables[0].DefaultView;
+            cmbSection.ValueMember = "id";
+            cmbSection.DisplayMember = "sectionname";
+            //cmbSection.SelectedIndex = 0;//默认选择第一项
+        }
+    }
+}
