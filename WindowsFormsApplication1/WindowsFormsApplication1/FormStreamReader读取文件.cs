ef8511d2662978a06fa4ee29b6e86559d9842ad0
sunlupeng2020 <slp2060@163.com>
2020-12-17 16:23:48 +0800

    添加了用StreamReader读取文本文件的窗体

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/FormStreamReader读取文件.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormStreamReader读取文件 : Form
+    {
+        public FormStreamReader读取文件()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string line;
+            string path = textBox1.Text.Trim();
+            try
+            {
+                FileStream aFile = new FileStream(path, FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    textBox2.Text += line;
+                    line = sr.ReadLine();
+                }
+                sr.Close();//关闭流对象
+            }
+            catch (IOException ex)//
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
